c4296e1266d885fd84084b0c04613b635a759556
Erico-AS <ericoaugustosstj@gmail.com>
2023-01-20 16:28:30 -0300

    Variáveis

--- prj-1/src/TiposVariaveis.java
+++ prj-1/src/TiposVariaveis.java
@@ -1,5 +1,19 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double salarioMinimo = 2500;
+
+        byte idade = 123;
+        short ano = 2021;
+        int cep = 21891070;
+        long cpf = 10023456721l;
+        final float PI = 3.14F; // 'final' indica que seu valor não muda, semelhante a uma 'const'
+
+        System.out.println(idade + " " + ano + " " + cep);
+        System.out.println(cpf + " " + PI + " " + salarioMinimo);
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        //Adição do 'short' para pegar um valor abrangete e diminuir para um mais específico
+        short numeroCurto2 = (short) numeroNormal;
     }
 }
