7a679526718b95c9ca9ea5a1314c7487ba9acd8a
Erico-AS <ericoaugustosstj@gmail.com>
2023-01-20 17:03:23 -0300

    Operadores

---
+++ prj-1/src/Operadores.java
@@ -0,0 +1,8 @@
+public class Operadores {
+    public static void main(String [] args) {
+        int numero = 10;
+
+        System.out.println(- numero); // Inverte negativamente
+        System.out.println(+ numero); // Deixa positivo
+    }
+}
